fb163e6861e816e0a582f597a1dc376c981c85ba
sultanpernandoputra <147905965+sultanpernandoputra@users.noreply.github.com>
2023-12-08 17:02:59 +0700

    progres edit data
    progres edit data

--- coding/index.php
+++ coding/index.php
@@ -3,35 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form Transaksi Cucian</title>
+    <title>Input Edit Data</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <div class="container">
+    <h2>Input Edit Data</h2>
+        <?php include('koneksi.php'); ?>
+        <form action="save_data.php" method="post">
+            <label for="nama">Nama:</label>
+            <input type="text" name="nama" required>
 
-<div class="container">
-    <h2>Form Transaksi Cucian</h2>
-    <form action="proses_transaksi.php" method="post">
-        <label for="nama">Nama :</label>
-        <input type="text" name="nama" required>
+            <label for="alamat">Alamat:</label>
+            <input type="text" name="alamat" required>
 
-        <label for="alamat">Alamat :</label>
-        <input type="text" name="alamat" required>
+            <label for="telepon">Telepon:</label>
+            <input type="tel" name="telepon" required>
 
-        <label for="berat">Berat Cucian (kg) :</label>
-        <input type="number" name="berat" required>
+            <label for="username">Username:</label>
+            <input type="text" name="username" required>
 
-        <label for="paket">Jenis Paket :</label>
-        <select name="paket" required>
-            <option value="reguler">Reguler</option>
-            <option value="express">Express</option>
-        </select>
+            <label for="password">Password:</label>
+            <input type="password" name="password" required>
 
-        <label for="tgl_pengambilan">Tanggal Pengambilan :</label>
-        <input type="date" name="tgl_pengambilan" required>
-
-        <button type="submit">Submit</button>
-    </form>
-   
-</div>
+            <button type="submit">Simpan Data</button>
+        </form>
+    </div>
 </body>
 </html>
--- coding/koneksi.php
+++ coding/koneksi.php
@@ -1,3 +1,14 @@
 <?php
-	$conn = mysqli_connect('localhost', 'root', '', 'laundryIPL');
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'cucian_db';
+
+// Lakukan koneksi ke database
+$conn = new mysqli($hostname, $username, $password, $database);
+
+// Periksa koneksi
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
